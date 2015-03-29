217468bc66a1eb075305d3d8db318a22b9114c06
Maria <meridelapuerta@gmail.com>
2015-03-29 18:36:05 -0300

    new twelve block board

--- src/boards/BoardGenerator.java
+++ src/boards/BoardGenerator.java
@@ -211,4 +211,78 @@ public class BoardGenerator {
 		return new Board(board, blocks, finalSquare);
 	}
 
+	public static Board getTwelveBlockBoard() {
+		int[][] board = new int[6][6];
+		List<Block> blocks = new ArrayList<Block>();
+		
+		board[2][0] = 1;
+		board[2][1] = 1;
+		
+		board[0][1] = 2;
+		board[1][1] = 2;
+		
+		board[0][2] = 3;
+		board[0][3] = 3;
+		
+		board[0][4] = 4;
+		board[0][5] = 4;
+		
+		board[1][4] = 5;
+		board[1][5] = 5;
+		
+		board[1][3] = 6;
+		board[2][3] = 6;
+		
+		board[4][2] = 7;
+		board[5][2] = 7;
+		
+		board[2][5] = 8;
+		board[3][5] = 8;
+		
+		board[4][5] = 9;
+		board[5][5] = 9;
+		
+		board[3][0] = 10;
+		board[4][0] = 10;
+		board[5][0] = 10;
+		
+		board[3][1] = 11;
+		board[3][2] = 11;
+		board[3][3] = 11;
+		
+		board[2][4] = 12;
+		board[3][4] = 12;
+		board[4][4] = 12;
+		
+		Block b1 = new Block(1, new Square(2, 1), new Square(2, 0), true, 2);
+		Block b2 = new Block(2, new Square(1, 1), new Square(0, 1), false, 2);
+		Block b3 = new Block(3, new Square(0, 3), new Square(0, 2), true, 2);
+		Block b4 = new Block(4, new Square(0, 5), new Square(0, 4), true, 2);
+		Block b5 = new Block(5, new Square(1, 5), new Square(1, 4), true, 2);
+		Block b6 = new Block(6, new Square(2, 3), new Square(1, 3), false, 2);
+		Block b7 = new Block(7, new Square(5, 2), new Square(4, 2), false, 2);
+		Block b8 = new Block(8, new Square(3, 5), new Square(2, 5), false, 2);
+		Block b9 = new Block(9, new Square(5, 5), new Square(4, 5), false, 2);
+		Block b10 = new Block(10, new Square(5, 0), new Square(3, 0), false, 3);
+		Block b11 = new Block(11, new Square(3, 3), new Square(3, 1), true, 3);
+		Block b12 = new Block(12, new Square(4, 4), new Square(2, 4), false, 3);
+		
+		blocks.add(b1);
+		blocks.add(b2);
+		blocks.add(b3);
+		blocks.add(b4);
+		blocks.add(b5);
+		blocks.add(b6);
+		blocks.add(b7);
+		blocks.add(b8);
+		blocks.add(b9);
+		blocks.add(b10);
+		blocks.add(b11);
+		blocks.add(b12);
+
+		Square finalSquare = new Square(2, 5);
+		
+		return new Board(board, blocks, finalSquare);
+
+	}
 }
